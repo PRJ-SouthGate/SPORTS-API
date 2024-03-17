40e6e2f630eecb129db21c01b1e482d680754573
heize0502 <heize0502@naver.com>
2024-03-17 22:27:05 +0900

    3.17 10:26

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+
+const app = express();
 app.use(express.json()); // JSON 요청 본문 처리
 app.use(express.urlencoded({ extended: true }));
-const soccerRoutes = require('./routes/soccerRoutes');
-app.use('/soccer', soccerRoutes);
-const PORT = process.env.PORT
-app.set('view engine', 'ejs')
+const soccerRoutes = require("./routes/soccerRoutes");
+app.use("/soccer", soccerRoutes);
+const PORT = process.env.PORT;
+app.set("views", path.join(__dirname, "./views"));
+app.set("view engine", "ejs");
 
-
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (req, res) => {
+    res.render("index");
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
