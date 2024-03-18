c3f0da4a4cbf093ef47a524ffddd62760444cdac
heize0502 <heize0502@naver.com>
2024-03-19 01:21:36 +0900

    3.19. 버그 수정 및 insert 구현

--- src/models/soccerModels/playerStatsModel.js
+++ src/models/soccerModels/playerStatsModel.js
@@ -1,39 +1,124 @@
-const db = require('../../db/database');
+const db = require("../../db/database");
+
+async function insertPlayerStats(stats) {
+    // stats 객체를 인자로 받아서 데이터베이스에 삽입하는 함수
+    const {
+        // stats 객체에서 필요한 속성들을 추출
+        playerId,
+        season,
+        goals,
+        assists,
+        rating,
+        teamId,
+        name,
+        leagueId,
+        appearances,
+        minutesPlayed,
+        yellowCards,
+        redCards,
+        lineups,
+        firstname,
+        lastname,
+        age,
+        birth_date,
+        nationality,
+        height,
+        weight,
+        photo,
+    } = stats;
+    const query_stats = `
+        INSERT INTO player_stats
+        (playerId, season, goals, assists, rating, teamId, leagueId, appearances, minutesPlayed, yellowCards, redCards, lineups)
+        VALUES
+        (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+    `; // 쿼리 템플릿
+    const query_players = `
+        INSERT INTO players (id, name, firstname, lastname, age, birth_date, nationality, height, weight, photo)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+    `; // 쿼리 템플릿
+    console.log("Inserting player stats", stats);
+    try {
+        const [result] = await db.execute(query_players, [
+            playerId,
+            name,
+            firstname,
+            lastname,
+            age,
+            birth_date,
+            nationality,
+            height,
+            weight,
+            photo,
+        ]);
+        console.log("Inserting successful", result);
+    } catch (error) {
+        console.error("Error inserting player stats", error);
+    }
+    try {
+        const [result] = await db.execute(query_stats, [
+            playerId,
+            season,
+            goals,
+            assists,
+            rating,
+            teamId,
+            leagueId,
+            appearances,
+            minutesPlayed,
+            yellowCards,
+            redCards,
+            lineups,
+        ]);
+        console.log("Inserting successful", result);
+    } catch (error) {
+        console.error("Error inserting player stats", error);
+    }
+}
 
 async function updatePlayerStats(stats) {
-    const { playerId, season, goals, assists, appearances } = stats;
+    // stats 객체를 인자로 받아서 데이터베이스에 업데이트하는 함수
+    const { playerId, season, goals, assists, rating } = stats; // stats 객체에서 필요한 속성들을 추출
 
     const query = `
         UPDATE player_stats
-        SET goals = ?, assists = ?, appearances = ?
+        SET goals = ?, assists = ?, rating = ?
         WHERE playerId = ? AND season = ?;
-    `;
+    `; // 쿼리 템플릿
 
     try {
-        const [result] = await db.execute(query, [goals, assists, appearances, playerId, season]);
-        console.log('Update successful', result);
+        const [result] = await db.execute(query, [
+            goals,
+            assists,
+            rating,
+            playerId,
+            season,
+        ]);
+        console.log("Update successful", result);
     } catch (error) {
-        console.error('Error updating player stats', error);
+        console.error("Error updating player stats", error);
     }
 }
 
 async function getPlayerStats(playerName) {
+    // 플레이어 이름을 인자로 받아서 해당 플레이어의 통계를 반환하는 함수
     const query = `
-        SELECT ps.season, ps.goals, ps.assists, ps.rating,t.name AS teamName
+        SELECT ps.season, ps.goals, ps.assists, ps.rating, t.name AS teamName, p.name AS playerName
         FROM player_stats ps
         JOIN teams t ON ps.teamId = t.id
+        JOIN players p ON ps.playerId = p.id
         WHERE ps.playerId = (
-            SELECT id FROM players WHERE name = ?
+            SELECT id FROM players WHERE name LIKE CONCAT('%', ?, '%')
         )
-    `;
+    `; // 쿼리 템플릿
 
     try {
         const [rows, fields] = await db.query(query, [playerName]);
+        console.log("Fetching player stats", rows);
         return rows;
     } catch (error) {
-        console.error('Error fetching player stats', error);
+        console.error("Error fetching player stats", error);
         return null;
     }
 }
 
-module.exports = { updatePlayerStats, getPlayerStats };
+module.exports = { updatePlayerStats, getPlayerStats, insertPlayerStats };
--- src/routes/soccerRoutes.js
+++ src/routes/soccerRoutes.js
@@ -1,41 +1,62 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { getPlayerStats } = require('../models/soccerModels/playerStatsModel'); // 경로는 실제 구조에 맞게 조정
-const { updatePlayerStats } = require('../models/soccerModels/playerStatsModel');
-const { fetchPlayerStats } = require('../api/footballStatsAPI');
+const { getPlayerStats } = require("../models/soccerModels/playerStatsModel"); // 경로는 실제 구조에 맞게 조정
+const {
+    updatePlayerStats,
+} = require("../models/soccerModels/playerStatsModel");
+const { fetchPlayerStats } = require("../api/footballStatsAPI");
+const {
+    insertPlayerStats,
+} = require("../models/soccerModels/playerStatsModel");
 
-
-// 선수 프로필 페이지 라우트
-router.get('/player/:playerName', async (req, res) => {
+router.get("/player/:playerName", async (req, res) => {
+    // 선수 프로필 페이지 라우트
     const playerName = req.params.playerName;
-    const leagueId = req.query.leagueId
-    console.log(leagueId + typeof (leagueId));
+    const leagueId = req.query.leagueId;
     try {
         const playerStats = await getPlayerStats(playerName);
 
         if (playerStats && playerStats.length > 0) {
             // 플레이어 통계 정보가 있는 경우, 프로필 페이지 렌더링
-            res.render('playerProfile', { playerName: playerName, stats: playerStats, leagueId: leagueID });
+            res.render("playerProfile", {
+                playerName: playerName,
+                stats: playerStats,
+                leagueId: leagueId,
+                existence: true,
+            });
         } else {
             // 플레이어 통계 정보가 없는 경우, 사용자에게 알림
-            res.render('playerNotFound', { playerName: playerName, leagueId: leagueId });
+            res.render("playerNotFound", {
+                playerName: playerName,
+                leagueId: leagueId,
+                existence: false,
+            });
         }
     } catch (error) {
         console.error(error);
-        res.status(500).send('Internal Server Error');
+        res.status(500).send("Internal Server Error");
     }
 });
 
-router.post('/updatePlayerStats', async (req, res) => {
-    const playerName = req.body.playerName;
-    const leagueID = req.body.leagueId;
+router.post("/updatePlayerStats", async (req, res) => {
+    // 플레이어 통계 정보 업데이트 라우트
+    const playerName = req.body.playerName; // 이 이름으로 api에서 검색하는데 검색이 되는 이름이 명확하지 않음 추후 확인후 수정필요
+    const leagueId = req.body.leagueId;
+    const existence = req.body.existence;
     try {
-        const stats = await fetchPlayerStats(playerName, leagueID);
-        await updatePlayerStats(stats);
-        res.redirect(`/player/${playerName}`);
+        const stats = await fetchPlayerStats(playerName, leagueId);
+        if (existence === "true") {
+            // 플레이어 통계 정보가 이미 있는 경우
+            await updatePlayerStats(stats); // 기존 정보를 업데이트
+            res.redirect(`/soccer/player/${playerName}?leagueId=${leagueId}`);
+        } else {
+            // 플레이어 통계 정보가 없는 경우
+            await insertPlayerStats(stats); // 새로운 정보를 삽입
+            res.redirect(`/soccer/player/${playerName}?leagueId=${leagueId}`);
+        }
     } catch (error) {
         console.error(error);
-        res.status(500).send('Error updating player stats');
+        res.status(500).send("Error updating player stats");
     }
 });
 
