f1dafe7721c75bbb49e3d332f27d8d443c44c68b
Juseong <js22211@naver.com>
2024-03-25 00:12:00 +0900

    0325 수정

--- src/routes/soccerRoutes.js
+++ src/routes/soccerRoutes.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const router = express.Router();
-const { getPlayerStats } = require("../models/soccerModels/playerStatsModel"); // 경로는 실제 구조에 맞게 조정
+const { getPlayerStatsByName } = require("../models/soccerModels/playerStatsModel"); // 경로는 실제 구조에 맞게 조정
 const {
-    updatePlayerStats,
+    updatePlayerStats
 } = require("../models/soccerModels/playerStatsModel");
-const { fetchPlayerStats } = require("../api/footballStatsAPI");
+const { fetchPlayerStatsById } = require("../api/footballStatsAPI");
+const { fetchPlayerStatsByName } = require("../api/footballStatsAPI");
 const {
-    insertPlayerStats,
+    insertPlayerStats
 } = require("../models/soccerModels/playerStatsModel");
+const { getPlayerIdByName } = require('../models/soccerModels/playerStatsModel');
 
 router.get("/player/:playerName", async (req, res) => {
     // 선수 프로필 페이지 라우트
     const playerName = req.params.playerName;
     const leagueId = req.query.leagueId;
     try {
-        const playerStats = await getPlayerStats(playerName);
-
+        const playerStats = await getPlayerStatsByName(playerName);
         if (playerStats && playerStats.length > 0) {
             // 플레이어 통계 정보가 있는 경우, 프로필 페이지 렌더링
             res.render("playerProfile", {
@@ -40,20 +41,25 @@ router.get("/player/:playerName", async (req, res) => {
 
 router.post("/updatePlayerStats", async (req, res) => {
     // 플레이어 통계 정보 업데이트 라우트
-    const playerName = req.body.playerName; // 이 이름으로 api에서 검색하는데 검색이 되는 이름이 명확하지 않음 추후 확인후 수정필요
+    const playerName = req.body.playerName;
+    console.log(playerName);
     const leagueId = req.body.leagueId;
-    const existence = req.body.existence;
+    const playerId = await getPlayerIdByName(playerName); // 이 이름으로 api에서 검색하는데 검색이 되는 이름이 명확하지 않음 추후 확인후 수정필요
+    console.log(playerId);
     try {
-        const stats = await fetchPlayerStats(playerName, leagueId);
-        if (existence === "true") {
-            // 플레이어 통계 정보가 이미 있는 경우
-            await updatePlayerStats(stats); // 기존 정보를 업데이트
-            res.redirect(`/soccer/player/${playerName}?leagueId=${leagueId}`);
+        
+        if (playerId) {
+            // ID가 있으면 외부 API로부터 통계를 가져옴
+            const stats = await fetchPlayerStatsById(playerId, leagueId);
+        console.log(stats);
+            await updatePlayerStats(stats); // DB에 통계를 업데이트함
         } else {
-            // 플레이어 통계 정보가 없는 경우
-            await insertPlayerStats(stats); // 새로운 정보를 삽입
-            res.redirect(`/soccer/player/${playerName}?leagueId=${leagueId}`);
+            // ID가 없으면 외부 API에서 선수의 통계를 가져옴
+            const stats = await fetchPlayerStatsByName(playerName, leagueId);
+        console.log(stats);
+            await insertPlayerStats(stats); // 새로운 통계를 DB에 삽입함
         }
+        res.redirect(`/soccer/player/${playerName}?leagueId=${leagueId}`);
     } catch (error) {
         console.error(error);
         res.status(500).send("Error updating player stats");
