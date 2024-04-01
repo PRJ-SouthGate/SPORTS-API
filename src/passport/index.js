f19c9146cbf823c2bc93e50a556dfc97750cd7d2
heize0502 <heize0502@naver.com>
2024-04-01 21:41:38 +0900

    3/31 로그인 로그아웃 구현

--- src/passport/index.js
+++ src/passport/index.js
@@ -1,15 +1,18 @@
-const kakaoStrategy = require('./kakaoStrategy');
-const User = require('../models/userModels/userModel');
+const kakaoStrategy = require("./kakaoStrategy");
+const { findUserById } = require("../models/userModels/userModel");
 
 module.exports = (passport) => {
     passport.serializeUser((user, done) => {
-        done(null, user.snsId); // 사용자의 ID만 세션에 저장
+        console.log("serializeUser", user);
+        done(null, { id: user.snsId, accessToken: user.accessToken }); // 사용자의 ID만 세션에 저장
     });
 
-    passport.deserializeUser(async (snsId, done) => { // 세션에 저장된 ID로 사용자 정보 조회
+    passport.deserializeUser(async (user, done) => {
+        // 세션에 저장된 ID로 사용자 정보 조회
         try {
-            const user = await User.findUserById(snsId); // 사용자 정보를 데이터베이스에서 조회하는 메소드
-            done(null, user); // 조회된 사용자 정보를 req.user에 저장
+            const dbUser = await findUserById(user.id); // 사용자 정보를 데이터베이스에서 조회하는 메소드
+            const userInfo = { ...dbUser, accessToken: user.accessToken };
+            done(null, userInfo); // 조회된 사용자 정보를 req.user에 저장
         } catch (error) {
             console.error(error);
             done(error);
--- src/passport/kakaoStrategy.js
+++ src/passport/kakaoStrategy.js
@@ -1,30 +1,41 @@
-
-const KakaoStrategy = require('passport-kakao').Strategy
-const User = require('../models/userModels/userModel');
+const KakaoStrategy = require("passport-kakao").Strategy;
+const { findUserById, createUser } = require("../models/userModels/userModel");
 
 module.exports = (passport) => {
-
-    passport.use(new KakaoStrategy({
-        clientID: process.env.KAKAO_ID, //kakao에서 발급해주는 id, .env 파일 생성후 KAKAO_ID:카카오에서 발급해준 api 를 추가해주면 된다.
-        callbackURL: '/auth/kakao/callback', //인증결과를 받는 라우터이다.
-    }, async (accessToken, refreshToken, profile, done) => {
-        try {
-            const exUser = await User.findUserById(profile.id);
-            if (exUser) {
-                done(null, exUser);
-            } else {
-                const newUser = await User.createUser({ //새 유저 생성
-                    //닉네임, 카카오 id 숫자, 제공자 저장
-                    nickname: profile.displayName,
-                    snsId: profile.id,
-                    provider: 'kakao',
-                });
-                done(null, newUser);
+    passport.use(
+        new KakaoStrategy(
+            {
+                clientID: process.env.KAKAO_ID, //kakao에서 발급해주는 id, .env 파일 생성후 KAKAO_ID:카카오에서 발급해준 api 를 추가해주면 된다.
+                callbackURL: "/auth/kakao/callback", //인증결과를 받는 라우터이다.
+            },
+            async (accessToken, refreshToken, profile, done) => {
+                try {
+                    const exUser = await findUserById(profile.id);
+                    if (exUser) {
+                        const tokenUser = {
+                            ...exUser,
+                            accessToken: accessToken,
+                        };
+                        done(null, tokenUser);
+                    } else {
+                        const newUser = await createUser({
+                            //새 유저 생성
+                            //닉네임, 카카오 id 숫자, 제공자 저장
+                            nickname: profile.displayName,
+                            snsId: profile.id,
+                            provider: "kakao",
+                        });
+                        const tokenUser = {
+                            user: newUser,
+                            accessToken: accessToken,
+                        };
+                        done(null, tokenUser);
+                    }
+                } catch (error) {
+                    console.error(error);
+                    done(error);
+                }
             }
-        } catch (error) {
-            console.error(error);
-            done(error);
-        }
-    }));
-
+        )
+    );
 };
